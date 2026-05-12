03ddc4316b46262399288c17baa5379801c0ec98
André IKOTO <126103789+ikoto92@users.noreply.github.com>
2026-05-12 15:18:58 +0200

    Update Program.cs

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -6,6 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //
         string directoryPath = "file";
 
         string csvFilePath = "results.csv";
@@ -23,24 +24,18 @@ class Program
         {
             Console.WriteLine("Veuillez entrer un nombre valide (1, 2, 3 ou 4) :");
         }
-
-
         string[] fileNames = {"08b49b5f34fdc2c28191b45749c365a8.txt", "0e303ef7354aab60637e09a5fae96fa8.txt", "9264a7c4e7251fc75910c4155e5c489a.txt", "e001e8d4317c1db1d6a0f3d3f6d03d25.txt" };
-
         string fileName = fileNames[choice - 1];
         string filePath = Path.Combine(directoryPath, fileName);
-
         if (File.Exists(filePath))
         {
             List<string> words = new List<string>(File.ReadAllLines(filePath));
-
             Action<List<string>>[] sortingAlgorithms = {
                 SortingAlgorithms.SelectionSort,
                 SortingAlgorithms.BubbleSort,
                 SortingAlgorithms.InsertionSort,
                 SortingAlgorithms.QuickSort
             };
-
             foreach (var algorithm in sortingAlgorithms)
             {
                 List<string> wordsCopy = new List<string>(words);
@@ -48,14 +43,12 @@ class Program
                 double executionTime = Benchmark.MeasureSortingTime(algorithm, wordsCopy);
                 csvFile.WriteLine($"{algorithm.Method.Name},{executionTime}");
             }
-
             Console.WriteLine("Les résultats ont été enregistrés dans le fichier CSV.");
         }
         else
         {
             Console.WriteLine($"Le fichier {fileName} n'existe pas dans le répertoire.");
         }
-
         csvFile.Close();
     }
 }
